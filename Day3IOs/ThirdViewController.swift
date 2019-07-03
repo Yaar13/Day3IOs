ab3683e0eb1d7711b825f15c97ba8ce0ffd4f744
parag <paraggarg66@yahoo.in>
2019-07-03 18:18:37 -0400

    how to use Picture icon and Navigation Control

---
+++ Day3IOs/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Day3IOs
+//
+//  Created by Gurparkash Kaur on 2019-07-02.
+//  Copyright © 2019 Parag Garg. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.backItem?.hidesBackButton
+        = false
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Day3IOs/ViewController.swift
+++ Day3IOs/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageJungle: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Sliderbtn(_ sender: Any) {
+    }
+    @IBOutlet weak var LblMessage: UILabel!
+    @IBAction func SetImage(_ sender: Any) {
+       ImageJungle.image = UIImage(named: "gorilla.png")
+        ImageJungle.image = #imageLiteral(resourceName: "ice cream.png")
+        
+    }
+    @IBAction func Swtchbutton(_ sender: Any) {
+    func Swtchbutton(_ sender: UISwitch) {
+        if sender.isOn {
+            ImageJungle.image = UIImage(named: "elephant")
+        }
+        else {
+            ImageJungle.image = UIImage(named: "gorilla.png")
+        }
+    }
 }
 
+}
